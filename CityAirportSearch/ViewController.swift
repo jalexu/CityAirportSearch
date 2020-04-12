cdbcdf6a0635bf7ffce2423ca6abef1948161704
Jaime Alexander Uribe <jaimealexanderuribe@gmail.com>
2020-04-11 21:18:22 -0500

    Initial Commit

---
+++ CityAirportSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityAirportSearch
+//
+//  Created by Jaime Uribe on 11/04/20.
+//  Copyright © 2020 Jaime Uribe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
